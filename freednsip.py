4af55a06c5595adb8518a6c0415f2deb9db23cb1
livyme <li.livy@gmail.com>
2013-12-24 15:59:37 -0600

    removed curl, using python httplib

--- freednsip.py
+++ freednsip.py
@@ -6,89 +6,101 @@
 #  Created by Livy on 2013-12-23.
 #  Copyright 2013 Livy. All rights reserved.
 #  
-
-import subprocess
+# 
+import httplib
 import smtplib
 import time
 from email.mime.text import MIMEText
 
 ipFile = '/usr/local/freednsip/ip.txt'
+freeDNSHost = 'freedns.afraid.org'
 mbpFreeDNSString = 'VURmdmdhMzFVMVVBQU9IVVN5MEFBQUFIOjg0NDc2ODg='
 centralFreeDNSString = 'VURmdmdhMzFVMVVBQU9IVVN5MEFBQUFIOjgyMDY3NTc='
-dnsURL = 'http://freedns.afraid.org/dynamic/update.php?'+ mbpFreeDNSString
+dnsURL = '/dynamic/update.php?'+ mbpFreeDNSString
 selfName = 'Macbook Pro Retina'
 
 # Get IP Address
-currentIP = subprocess.check_output(['curl', '--silent', 'http://ipecho.net/plain'])
-
-# Read IP from ip.txt, if not exists then create it.
 try:
-    f = open(ipFile,'r')
-except IOError:
-    print ('no ip file exsits, creating it')
-    subprocess.call(['touch', ipFile])
-    f = open(ipFile,'r')
-previousIP = f.read()
-f.close()
+    conn = httplib.HTTPConnection('ipecho.net')
+    conn.request('GET', '/plain')
+    r1 = conn.getresponse()
+    # print 'status: ', r1.status
+    # print 'reason: ', r1.reason
+    currentIP = r1.read()
+    conn.close()
+    # Read previously recorded IP from ipFile
+    # If file not exists then create it.
+    try:
+        f = open(ipFile,'r')
+    except IOError:
+        print ('no ip file exsits, creating it')
+        f = open(ipFile,'w+')
+    
+    previousIP = f.read()
+    f.close()
 
-if previousIP == currentIP:
-    print 'IP not changed, nothing to do. \nCurrent IP is', currentIP
-else:
-    print 'IP changed to ', currentIP
+    if previousIP == currentIP:
+        print 'IP not changed, nothing to do. \nCurrent IP is', currentIP
+    else:
+        print 'IP changed to ', currentIP
     
-    # Change FreeDNS registration
-    # subprocess.call(['curl', '-k', '--silent', dnsURL])
-    
-    # Some weird error occurs when contacting FreeDNS server.
-    # So when receiving a 502 bad gateway error
-    
-    while True:
-        proc = subprocess.Popen(['curl', '-k', '--silent', dnsURL], stdout=subprocess.PIPE)
-        (out,err) = proc.communicate()
+        # Change FreeDNS registration
+        while True:
+            conn = httplib.HTTPConnection(freeDNSHost)
+            conn.request('GET', dnsURL)
+            r1 = conn.getresponse()
+            result = r1.read()
+            conn.close()
+            
+            if r1.status == 200:
+                if 'Updated' in result:
+                    f = open(ipFile,'w')
+                    f.write(currentIP)
+                    f.close()
+                    print 'Update dns successful\n',result
+                elif 'has not changed' in result:
+                    print result
+                    # Although no change, but go ahead and change the IP file anyway
+                    f = open(ipFile,'w')
+                    f.write(currentIP)
+                    f.close()
+                else:
+                    print 'Unknown error\n',result
+                break
+            else:
+                print 'Got resonse from server: ', result, '\n Retry after 20 sec...'
+                time.sleep(20)
+
+        # Send notifications if ip changes...
+        # Prepare message
+        recipients = ['7854326040@txt.att.net', 'i@livyme.com']
+        msg = MIMEText('Public IP address for '+ selfName +' has changed. \nNew IP: ' + currentIP)
+        msg['From'] = 'root'
+        msg['To'] = ', '.join(recipients)
+        msg['Subject'] = selfName + ' IP Change'
+        # Optional header fields just for fun
+        msg['X-Priority'] = '1'
+        msg['X-Message-Flag'] = 'Livyme'
+        msg['X-Generated-By'] = 'Python'
+        msg['Importance'] = 'High'
         
-        if 'Updated' in out:
-            f = open(ipFile,'w')
-            f.write(currentIP)
-            f.close()
-            print 'Update dns successful'
-            break
-        elif 'has not changed' in out:
-            print 'Address in DNS does not appear to have changed.'
-            break
-        elif '502 Bad Gateway' in out:
-            print '502 Bad Gateway code, retry after 20 seconds...'
-            time.sleep(20)
-        else:
-            print 'Unknown error'
-            break
-    
-    # Send notifications if ip changes...
-    # Prepare message
-    recipients = ['7854326040@txt.att.net', 'i@livyme.com', 'li.livy@gmail.com']
-    msg = MIMEText('Public IP address for '+ selfName +' has changed. \nNew IP: ' + currentIP)
-    msg['From'] = 'root'
-    msg['To'] = ', '.join(recipients)
-    msg['Subject'] = selfName + ' IP Change'
-    # Optional header fields just for fun
-    msg['X-Priority'] = '1'
-    msg['X-Message-Flag'] = 'Livyme'
-    msg['X-Generated-By'] = 'Python'
-    msg['Importance'] = 'High'
-    
-    ## Send out using sendmail
-    ## p = subprocess.Popen(["/usr/sbin/sendmail", "-t"], stdin=subprocess.PIPE)
-    ## p.communicate(msg.as_string())
-    
-    # Send out using gmail
-    # Credentials
-    username = 'alert@livyme.com'
-    password = 'jae1EGhu'
-    
-    # Mail send using alert
-    fromAddr = 'alert@livyme.com'
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username,password)
-    server.sendmail(fromAddr, recipients, msg.as_string())
-    server.quit()
-    print 'Notification email sent out to admins.'
+        ## Send out using sendmail
+        ## p =subprocess.Popen(["/usr/sbin/sendmail", "-t"], stdin=subprocess.PIPE)
+        ## p.communicate(msg.as_string())
+        
+        # Send out using gmail
+        # Credentials
+        username = 'alert@livyme.com'
+        password = 'jae1EGhu'
+        
+        # Mail send using alert
+        fromAddr = 'alert@livyme.com'
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(fromAddr, recipients, msg.as_string())
+        server.quit()
+        print 'Notification email sent out to admins.'
+
+except Exception as e:
+    print e
