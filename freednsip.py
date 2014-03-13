649c7171e1872fe5d49c3b18cdc80ff7ff73f648
livyme <li.livy@gmail.com>
2014-03-13 16:13:21 -0500

    get rid of logging

--- freednsip.py
+++ freednsip.py
@@ -12,23 +12,19 @@ import urllib2
 import os.path
 import sys
 from os import sep
-import logging
 import ConfigParser
 
 def email_admin(mesg):
-  # Prepare message
   recipients = ['i@livyme.com',]
   msg = MIMEText(mesg)
   msg['From'] = 'root'
   msg['To'] = ', '.join(recipients)
   msg['Subject'] = config.get('me', 'name') + ' IP Change'
-  # Get gmail account info.
   username = config.get('gmail', 'username')
   password = config.get('gmail', 'password')
-  # Send
   server = smtplib.SMTP('smtp.gmail.com:587')
   server.starttls()
-  server.login(username,password)
+  server.login(username, password)
   server.sendmail(username, recipients, msg.as_string())
   server.quit()
 
@@ -47,64 +43,30 @@ def query_online(url):
   return (success, result)
 
 ipFile = sys.path[0]+sep+'ip.txt'
-logFile = sys.path[0]+sep+'freednsip.log'
 
 # Reading from setting.cfg file
 config = ConfigParser.RawConfigParser()
 config.read(sys.path[0]+sep+'settings.cfg')
 
-# Set up logging
-logLevel = config.get('logging', 'logLevel')
-consoleLogLevel = config.get('logging', 'consoleLogLevel')
-FORMAT = '%(asctime)s | %(levelname)-7s | %(message)s'
-logtemplate = '{0:<22s}\t{1:>15s}'
-logtemplatelong = logtemplate + '  ==>  {2}'
-logging.basicConfig(filename=logFile, level=logLevel, format=FORMAT)
-# display logging information in console.
-console = logging.StreamHandler(sys.stdout)
-console.setLevel(consoleLogLevel)
-console.setFormatter(logging.Formatter(FORMAT))
-logging.getLogger('').addHandler(console)
-
 # Get IP Address
-(success, IPresult) = query_online(config.get('publicIP','url'))
+(success, current_ip) = query_online(config.get('publicIP','url'))
 if not success:     
-  logging.error('[Public IP] {0}'.format(IPresult))
-elif IPresult == '209.114.127.125':  # Do not update when using VPN
-  logging.debug(logtemplate.format('Using VPN, skipping:', IPresult))
-  print logtemplate.format('Using VPN, skipping:', IPresult)
-else:   
-  currentIP = IPresult;
-  logging.debug(logtemplate.format('Current IP address is:', currentIP))
-  
-  # Read Previous IP from ipFile
+  sys.stderr.write('[Public IP] {0}'.format(IPresult))
+elif current_ip != '209.114.127.125':  # Do not update when using FHSU VPN
   if os.path.exists(ipFile):
     f = open(ipFile,'r')
-    previousIP = f.read().strip()
+    previous_ip = f.read().strip()
     f.close()
   else:
-    previousIP = 'IP file not found'
-  logging.debug(logtemplate.format('Previous IP as recorded:', previousIP))
+    previous_ip = 'IP file not found'
 
-  # See if IP changed...
-  if previousIP == currentIP:
-    logging.debug(logtemplate.format('IP address not changed:', currentIP))
-    print logtemplate.format('IP address not changed:', currentIP)
-  else:
-    logging.debug(logtemplatelong.format('IP changed:',previousIP, currentIP))
-    print logtemplatelong.format('IP changed:',previousIP, currentIP)
-    
-    # Update FreeDNS with current IP...
-    (success, freeDNSresult) = query_online(config.get('freeDNS','url'))
-    if 'Updated' in freeDNSresult or 'has not changed' in freeDNSresult:
-      logging.info('[FreeDNS] {0}'.format(freeDNSresult))
-      print '[FreeDNS] {0}'.format(freeDNSresult)
-      # Update IP file:
+  if previous_ip != current_ip:
+    (success, free_dns_result) = query_online(config.get('freeDNS','url'))
+    if 'Updated' in free_dns_result or 'has not changed' in free_dns_result:
       f = open(ipFile,'w')
-      f.write(currentIP)
+      f.write(current_ip)
       f.close()
       # Let the admins know by email
-      emailadmin('[FreeDNS] {0}'.format(freeDNSresult))
-      logging.debug('Notification email sent out to admins.')
+      email_admin('[FreeDNS] {0}'.format(free_dns_result))
     else:
-      logging.error('[FreeDNS] {0}'.format(freeDNSresult))
+      sys.stderr.write('[FreeDNS] {0}'.format(free_dns_result))
